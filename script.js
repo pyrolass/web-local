49f7fde5bd0cf91de9b7d2b43d0f63a9c7dabafc
Las <crowclaw935@gmail.com>
2021-01-17 19:13:14 +0300

    edit added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const EmployeeForm = document.getElementById("AddEmployeeForm");
-const EditEmployee = document.getElementById("EditEmployee");
 let allEmployess = JSON.parse(window.localStorage.getItem('user')) || [];
 let count =3;
 
 allEmployess.forEach(el => {
     renderTemplates(el);
 })
+
+
+
+
 document.querySelector("#username").innerHTML=localStorage.getItem("username")
 document.addEventListener('DOMContentLoaded', function () {
+
+   
+
 
     
     $(document).ready(function () {
@@ -34,21 +39,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-
+    let updateBtn = document.querySelector('#updateBtn');
     let addBtn = document.querySelector('#addBtn');
-
     let signOutBtn = document.querySelector('#signOutBtn');
+    let deleteBtn = document.querySelector('.deleteBtn')
 
     let nameInp = document.querySelector('#name');
     let emailInp = document.querySelector('#email');
-
     let phoneInp = document.querySelector('#phone');
+
+    let editName = document.querySelector('#editName');
+    let editPhone = document.querySelector('#editPhone');
+    let editEmail = document.querySelector('#editEmail');
 
     addBtn.addEventListener('click', () => {
         let name = nameInp.value;
         let email = emailInp.value;
         let phone = phoneInp.value;
-
 
 
         const employee = {
@@ -71,12 +78,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
         });
 
+
+
+    var table = document.querySelector('table'),rIndex;
+
+    for(i=0; i<table.rows.length;i++){
+
+        table.rows[i].onclick = function(){
+            rIndex=this.rowIndex;
+            index=rIndex;
+            console.log(rIndex);
+            document.querySelector('#editName').value = this.cells[1].innerHTML;
+            document.querySelector('#editEmail').value = this.cells[2].innerHTML;
+            document.querySelector('#editPhone').value = this.cells[3].innerHTML;
+        }
+    };
+
+   
+    function updateRow(){
+    table.rows[rIndex].cells[1].innerHTML = document.querySelector('#editName').value;
+    table.rows[rIndex].cells[2].innerHTML = document.querySelector('#editEmail').value;
+    table.rows[rIndex].cells[3].innerHTML = document.querySelector('#editPhone').value;
+}
+
+
+    
+
+    updateBtn.addEventListener('click', () => {
+        updateRow();
+
+        });    
+
+    document.getElementById("editEmployeeModal").addEventListener("click", function(event){
+  event.preventDefault()
 });
+
+    document.querySelector().addEventListener("click",function(event){
+
+    });
+
+
+
+    
+   
+});
+
 
 
 function renderTemplates(employee) {
     let table = document.querySelector('table');
-
+    count+=1
     let template = `
     <tr>
     <td>
@@ -95,3 +146,24 @@ function renderTemplates(employee) {
     `
     table.innerHTML += template;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
